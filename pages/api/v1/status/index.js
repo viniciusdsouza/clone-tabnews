1839fe6f104724ce53ba74a900eacb274a980b16
viniciusdsouza <vduartesantiago@gmail.com>
2025-12-13 17:48:58 -0300

    feat: finish `api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,31 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "são bão" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnResult = await database.query("SHOW max_connections;");
+  const databaseMaxConnValue = databaseMaxConnResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnResult = await database.query({
+    text: "SELECT cast(count(*) as int) FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnValue = databaseOpenedConnResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnValue),
+        opened_connections: databaseOpenedConnValue,
+      },
+    },
+  });
 }
 
 export default status;
